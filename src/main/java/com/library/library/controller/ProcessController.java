40598b07c70a00220b5c5b3f6ac8229c472592f5
h.aghili <h.aghili@ipaam.ir>
2025-08-02 16:08:13 +0330

    add asyc response to ProcessController

--- src/main/java/com/library/library/controller/ProcessController.java
+++ src/main/java/com/library/library/controller/ProcessController.java
@@ -2,6 +2,7 @@ package com.library.library.controller;
 
 import com.library.library.dto.ProcessStartRequest;
 import io.camunda.zeebe.client.ZeebeClient;
+import io.camunda.zeebe.client.api.response.ProcessInstanceEvent;
 import io.camunda.zeebe.client.api.response.ProcessInstanceResult;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -18,17 +19,23 @@ public class ProcessController {
     private final ZeebeClient zeebeClient;
 
     @PostMapping("/start")
-    public ResponseEntity<Map<String, Object>> startProcess(@RequestBody ProcessStartRequest request) {
+//    public ResponseEntity<Map<String, Object>> startProcess(@RequestBody ProcessStartRequest request) {
+        public ResponseEntity<String> startProcess(@RequestBody ProcessStartRequest request) {
         Duration timeout = Duration.ofSeconds(20);
         Map<String, Object> variables = Map.of("valueToCheck", request.getValueToCheck());
-        ProcessInstanceResult result = zeebeClient.newCreateInstanceCommand()
+//        ProcessInstanceEvent result = zeebeClient.newCreateInstanceCommand()
+        ProcessInstanceEvent instance = zeebeClient.newCreateInstanceCommand()
+
+//        ProcessInstanceResult result = zeebeClient.newCreateInstanceCommand()
                 .bpmnProcessId("timeOut")
                 .latestVersion()
                 .variables(variables)
-                .withResult()
-                .requestTimeout(timeout)  // Set timeout explicitly
+//                .withResult()
+//                .requestTimeout(timeout)  // Set timeout explicitly
                 .send()
                 .join();
 
-        return ResponseEntity.ok(result.getVariablesAsMap());    }
+//        return ResponseEntity.ok(result.getVariablesAsMap());
+        return ResponseEntity.ok("Started process with instance key: " + instance.getProcessInstanceKey());
+    }
 }
